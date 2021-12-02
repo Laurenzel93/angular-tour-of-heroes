c7b0b73e1561a0a8112b7c42aa5aebf57b1e7e57
Lauren Wenzel <Laurenzel93@aol.com>
2021-12-02 12:16:19 -0500

    added list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Batman' },
+    { id: 12, name: 'Superman' },
+    { id: 13, name: 'Wonder Woman' },
+    { id: 14, name: 'Iron Man' },
+    { id: 15, name: 'The Hulk' },
+    { id: 16, name: 'Black Panther' },
+    { id: 17, name: 'Spider-Man' },
+    { id: 18, name: 'Thor' },
+    { id: 19, name: 'Hawkeye' },
+    { id: 20, name: 'Scarlet Witch' },
+];
